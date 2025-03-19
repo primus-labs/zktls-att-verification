e148a2bbc791bf7aebb73e488e74044fb90e3e0c
xiaojiqing <xiaojiqing@padolabs.org>
2025-03-19 01:18:01 -0700

    feat: verify partial http packet

--- src/aes_verification.rs
+++ src/aes_verification.rs
@@ -2,6 +2,7 @@ use serde::{Serialize, Deserialize};
 use aes_gcm::aead::KeyInit;
 use aes_gcm::{Aes128Gcm, AeadInPlace, Nonce};
 use hex;
+use aes::{Aes128, cipher::BlockEncrypt, cipher::generic_array::GenericArray};
 
 // TLS Record
 #[derive(Debug, Serialize, Deserialize)]
@@ -27,7 +28,31 @@ pub struct VerifyingData {
     pub packets: Vec<HTTPPacket> // HTTP Packet 
 }
 
-// verify full http packet ciphertext
+#[derive(Debug, Serialize, Deserialize)]
+pub struct BlockInfo {
+    pub id: usize,   // block id
+    pub mask: Vec<u8>  // block mask, 1u8 indicate this char is extracted
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TLSRecordOpt {
+    pub ciphertext: String,  // ciphertext in tls record, it is concated according to field `blocks`
+    pub nonce: String,  // nonce for decrypting the ciphertext
+    pub blocks: Vec<BlockInfo> // show how to construct the ciphertext. Note the length of ciphertext and the sum of the length of all bytes in all blocks should be equal
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HTTPPacketOpt {
+    pub aes_key: String,  // aes key for decrypting http packet
+    pub records: Vec<TLSRecordOpt>  // TLS Records, construct partial http packet
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct VerifyingDataOpt {
+    pub packets: Vec<HTTPPacketOpt> // partial HTTP Packet
+}
+
+// verify full http packet ciphertext, see `examples/full_http_responses.json` for the format of `json_content`
 pub fn verify_full_http_packet_ciphertext(json_content: String) -> Vec<String> {
     let verifying_data: VerifyingData = serde_json::from_str(&json_content).unwrap();
 
@@ -58,3 +83,77 @@ pub fn verify_full_http_packet_ciphertext(json_content: String) -> Vec<String> {
     all_packet
 }
 
+// increase the varying part of nonce
+fn incr_nonce(nonce: &mut [u8; 4]) {
+    let mut index: i8 = 3;
+    while index >= 0 {
+        if nonce[index as usize] == 255u8 {
+            nonce[index as usize] = 0;
+            index -= 1;
+        }
+        else {
+            nonce[index as usize] = nonce[index as usize] + 1;
+            break;
+        }
+    }
+}
+
+// compute necessary counter according `blocks`
+fn compute_counter(cipher: &Aes128, nonce: &Vec<u8>, blocks: &Vec<BlockInfo>, len: usize) -> Vec<u8>{
+    let mut result: Vec<u8> = vec![];
+    let mut nonce_index: [u8; 4] = [0u8; 4];
+
+    let block_len: Vec<usize> = blocks.iter().map(|info| info.mask.iter().sum::<u8>() as usize).collect();
+    let all_len: usize = block_len.iter().sum();
+    assert!(all_len == len);
+
+    let mut block_index: usize = 0;
+    incr_nonce(&mut nonce_index);
+    while result.len() < len {
+        incr_nonce(&mut nonce_index);
+        let nonce_u32: u32 = u32::from_be_bytes(nonce_index);
+        if nonce_u32 as usize == blocks[block_index].id + 2 {
+            let mask = &blocks[block_index].mask;
+            let mut full_nonce = nonce.clone();
+            full_nonce.extend(nonce_index);
+
+            let mut full_nonce = *GenericArray::from_slice(full_nonce.as_slice());
+            cipher.encrypt_block(&mut full_nonce);
+            let masked_data: Vec<u8> = full_nonce.into_iter().zip(mask.iter()).filter(|(_a, b)| *b == &1u8).map(|(a, _b)| a).collect();
+            result.extend(masked_data);
+
+            block_index += 1;
+        }
+    }
+    result
+}
+
+// verify partial http packet, See `examples/partial_http_responses.json` for the format of `json_content`
+pub fn verify_partial_http_packet_ciphertext(json_content: String) -> Vec<String> {
+    let verifying_data: VerifyingDataOpt = serde_json::from_str(&json_content).unwrap();
+
+    let mut all_packet = vec![];
+    for packet in verifying_data.packets.iter() {
+        let mut packet_msg: String = String::new();
+        let aes_key = &packet.aes_key;
+        let aes_key = hex::decode(aes_key).unwrap();
+        let aes_key = GenericArray::from_slice(&aes_key);
+
+        let cipher = Aes128::new(aes_key);
+
+        for record in packet.records.iter() {
+            let nonce = hex::decode(&record.nonce).unwrap();
+            let ciphertext = hex::decode(&record.ciphertext).unwrap();
+
+            let counters = compute_counter(&cipher, &nonce, &record.blocks, ciphertext.len());
+            assert!(ciphertext.len() == counters.len());
+
+            let decrypted_msg: Vec<u8> = counters.iter().zip(ciphertext.iter()).map(|(a, b)| a ^ b).collect();
+            let decrypted_msg = String::from_utf8_lossy(&decrypted_msg);
+            packet_msg += &decrypted_msg;
+
+        }
+        all_packet.push(packet_msg);
+    }
+    all_packet
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 mod aes_verification;
 use std::fs;
+use aes::{Aes128, cipher::{KeyInit, BlockEncrypt, BlockDecrypt, generic_array::GenericArray}};
+use rand::RngCore;
 
-fn test_aes_verification() {
+fn test_full_aes_verification() {
     let json_content = fs::read_to_string("./examples/full_http_responses.json").unwrap();
     println!("jsonContent: {}", json_content);
     let msg = aes_verification::verify_full_http_packet_ciphertext(json_content);
@@ -10,6 +12,36 @@ fn test_aes_verification() {
     }
 }
 
+fn test_partial_aes_verification() {
+    let json_content = fs::read_to_string("./examples/partial_http_responses.json").unwrap();
+    println!("jsonContent: {}", json_content);
+    let msg = aes_verification::verify_partial_http_packet_ciphertext(json_content);
+    for m in msg.iter() {
+        println!("decrypted msg: {}", m);
+    }
+}
+
+fn _test_aes_ecb() {
+    let mut key: [u8; 16] = [0u8; 16];
+    rand::thread_rng().fill_bytes(&mut key);
+    println!("key: {:?}", key);
+    let key = GenericArray::from_slice(&key);
+
+    let mut msg: [u8; 16] = [0u8; 16];
+    rand::thread_rng().fill_bytes(&mut msg);
+    println!("msg: {:?}", msg);
+    let mut msg = *GenericArray::from_slice(&msg);
+
+    let cipher = Aes128::new(key);
+    cipher.encrypt_block(&mut msg);
+    println!("ciphertext: {:?}", msg);
+
+    cipher.decrypt_block(&mut msg);
+    println!("decrypted: {:?}", msg);
+}
+
 fn main() {
-    test_aes_verification();
+    test_full_aes_verification();
+    // test_aes_ecb();
+    test_partial_aes_verification();
 }
